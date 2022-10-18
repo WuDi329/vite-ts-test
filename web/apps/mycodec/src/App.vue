6300bb5861bb1b9c0ada2d690df5894957c90968
wudi <wuuudiii@qq.com>
2022-10-18 19:16:56 +0800

    add codec worker

--- web/apps/mycodec/src/App.vue
+++ web/apps/mycodec/src/App.vue
@@ -15,13 +15,13 @@ import helloworld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <!-- <helloworld></helloworld> -->
+
   </div>
+  <helloworld></helloworld>
 
-
-  <Suspense>
+  <!-- <Suspense>
       <encode />
-  </Suspense>
+  </Suspense> -->
 
 
 </template>
